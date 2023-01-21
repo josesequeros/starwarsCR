4044fc49ff3d877f86dd1608af757b4de14e99dc
Jose Antonio <jose.sequeros@gmail.com>
2023-01-21 18:56:48 +0100

    V1.02 - Cambiada funcion mostrarTexto

--- src/starwarscodigossecretos/StarWarsCodigosSecretos.java
+++ src/starwarscodigossecretos/StarWarsCodigosSecretos.java
@@ -17,14 +17,13 @@ public class StarWarsCodigosSecretos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int respuestaNumero;
         boolean respuesta = false;
         int nivel = 0;
 
         Scanner entrada = new Scanner(System.in);
         System.out.print("Introduce el ancho del terreno: ");
 
-        respuestaNumero = mostrarTexto("Hace mucho tiempo, en una galaxia muy, muy lejana… \n"
+        mostrarTexto("Hace mucho tiempo, en una galaxia muy, muy lejana… \n"
                 + "La Princesa Leia, Luke Skywalker, Han Solo, Chewbacca, C3PO y R2D2 \n"
                 + "viajan en una nave imperial robada en una misión secreta para \n"
                 + "infiltrarse en otra estrella de la muerte que el imperio está \n"
@@ -57,10 +56,9 @@ public class StarWarsCodigosSecretos {
         //System.out.println(respuestaNumero);
     }
 
-    public static int mostrarTexto(String texto) {
+    public static void mostrarTexto(String texto) {
         System.out.println(texto);
         System.out.println("");
-        return 0;
     }
 
     public static boolean nivel1() {
